a00b5035dea34e994d06d2912d0bc0dfc014b487
Ean Jacob Gayban <ejacobg@outlook.com>
2023-02-21 11:04:09 -0800

    Finished 9.7, query supports pagination

--- internal/data/filters.go
+++ internal/data/filters.go
@@ -40,3 +40,10 @@ func (f Filters) sortDirection() string {
 	}
 	return "ASC"
 }
+
+func (f Filters) limit() int {
+	return f.PageSize
+}
+func (f Filters) offset() int {
+	return (f.Page - 1) * f.PageSize
+}
