c6a7555e7e9d16a24c678631e1eba28872532511
Ean Jacob Gayban <ejacobg@outlook.com>
2023-02-21 10:48:46 -0800

    Finished 9.6, query supports sorting movies

--- internal/data/filters.go
+++ internal/data/filters.go
@@ -1,6 +1,9 @@
 package data
 
-import "github.com/ejacobg/greenlight/internal/validator"
+import (
+	"github.com/ejacobg/greenlight/internal/validator"
+	"strings"
+)
 
 type Filters struct {
 	Page     int
@@ -16,4 +19,24 @@ func ValidateFilters(v *validator.Validator, f Filters) {
 	v.Check(f.PageSize > 0, "page_size", "must be greater than zero")
 	v.Check(f.PageSize <= 100, "page_size", "must be a maximum of 100")
 	v.Check(validator.In(f.Sort, f.SortSafelist...), "sort", "invalid sort value")
+}
+
+// sortColumn will extract the column value the Sort term refers to.
+// If the Sort term is invalid, this routine will panic.
+func (f Filters) sortColumn() string {
+	for _, safeValue := range f.SortSafelist {
+		if f.Sort == safeValue {
+			return strings.TrimPrefix(f.Sort, "-")
+		}
+	}
+	// The ValidateFilters call will check for correctness, but this is here just in case.
+	panic("unsafe sort parameter: " + f.Sort)
+}
+
+// sortDirection will return the appropriate direction keyword depending on if the '-' character is present.
+func (f Filters) sortDirection() string {
+	if strings.HasPrefix(f.Sort, "-") {
+		return "DESC"
+	}
+	return "ASC"
 }
