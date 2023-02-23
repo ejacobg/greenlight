7ad5a3e20eaca5a10c6e3dc21e8a12d6fb5b1459
Ean Jacob Gayban <ejacobg@outlook.com>
2023-02-23 13:20:27 -0800

    Finished 11.1, added global rate limiter

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"golang.org/x/time/rate"
 	"net/http"
 )
 
@@ -17,6 +18,21 @@ func (app *application) recoverPanic(next http.Handler) http.Handler {
 				app.serverErrorResponse(w, r, fmt.Errorf("%s", err))
 			}
 		}()
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (app *application) rateLimit(next http.Handler) http.Handler {
+	// Create a new rate limiter with a refill rate of 2 events/tokens per second, with a maximum capacity of 4 events/tokens.
+	limiter := rate.NewLimiter(2, 4)
+
+	// All calls to this function will pull from the above rate limiter.
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Check if there are enough tokens to perform an event, returning a 429 Too Many Requests response if there aren't.
+		if !limiter.Allow() {
+			app.rateLimitExceededResponse(w, r)
+			return
+		}
 		next.ServeHTTP(w, r)
 	})
 }
